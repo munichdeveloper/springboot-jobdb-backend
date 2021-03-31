d0936859ea5e0ae68cc0e3435745543230e1810b
Johannes Hinkov <johannes.hinkov@gmail.com>
2021-03-31 23:50:37 +0200

    add annotation to configure no job log

--- src/main/java/de/jgh/spring/jobdb/backend/jobdbbackend/annotation/NoJobLog.java
+++ src/main/java/de/jgh/spring/jobdb/backend/jobdbbackend/annotation/NoJobLog.java
@@ -1,4 +1,11 @@
 package de.jgh.spring.jobdb.backend.jobdbbackend.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface NoJobLog {
 }
